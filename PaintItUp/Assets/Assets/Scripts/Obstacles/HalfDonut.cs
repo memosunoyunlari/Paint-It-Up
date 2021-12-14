36d0599f7f37c9d00d3095bc8b7d163cb9f6a5ff
memosunoyunlari <93601245+memosunoyunlari@users.noreply.github.com>
2021-12-14 19:55:18 +0300

    5

--- PaintItUp/Assets/Assets/Scripts/Obstacles/HalfDonut.cs
+++ PaintItUp/Assets/Assets/Scripts/Obstacles/HalfDonut.cs
@@ -6,9 +6,11 @@ public class HalfDonut : MonoBehaviour
 {
     [SerializeField] GameObject stick;
 
-    [SerializeField] [Range(0.5f,1.5f)] float reachRange;
+    [SerializeField] [Range(0.1f,1.5f)] float reachRange;
 
     [SerializeField] [Range(0.25f, 1.5f)] float reachSpeed;
+
+    
 
     private Vector3 currentPos;
     private Vector3 targetPos;
@@ -17,12 +19,19 @@ public class HalfDonut : MonoBehaviour
 
     private void Awake()
     {
-        stick.transform.position = currentPos;
-        targetPos = new Vector3(reachRange + currentPos.x, currentPos.y, currentPos.z);
+        stick.transform.localPosition = currentPos;
+
+        targetPos = new Vector3(currentPos.x - reachRange, currentPos.y, currentPos.z);
+
+        
+        
+
     }
 
     private void Update()
     {
-        stick.transform.position = Vector3.Lerp(currentPos, targetPos, Mathf.PingPong(Time.time * reachSpeed, 1));
+        stick.transform.localPosition = Vector3.Lerp(currentPos, targetPos, Mathf.PingPong(Time.time * reachSpeed, 1));
     }
+
+    
 }
---
+++ PaintItUp/Assets/Assets/Scripts/Obstacles/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] [Range(15, 60)] float rotationSpeed;
+
+    [SerializeField] GameObject rotator;
+    [SerializeField] GameObject stick;
+
+    void Update()
+    {
+        rotator.transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        stick.transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+}
